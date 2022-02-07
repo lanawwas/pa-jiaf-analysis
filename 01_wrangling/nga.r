ca365cfb57cca321313468fec29d64b6e09bb332
turnerm <turnermon@gmail.com>
2022-02-07 16:32:49 +0100

    Made final df

--- 01_wrangling/nga.r
+++ 01_wrangling/nga.r
@@ -38,7 +38,51 @@ df_ocha_is <- df_ocha_raw %>%
   ) %>%
   mutate(sector = "intersectoral")
 
+df_ocha <- bind_rows(
+  df_ocha_clusters,
+  df_ocha_is
+) %>%
+  mutate(source = "ocha", .before = 1)
+
+
 ################
 #### PCODES ####
 ################
 
+df_pcodes <- read_excel(
+  fp_ocha,
+  sheet = "data",
+  skip = 1
+) %>%
+  select(
+      adm1_pcode = `state Pcode`,
+      adm1_en = State,
+      adm2_pcode = `LGA pcode`,
+      adm2_en = LGA
+  ) %>% unique()
+
+######################
+#### CLUSTER DATA ####
+######################
+
+# No cluster data
+
+############################
+#### GENERATE FULL DATA ####
+############################
+
+df_nga <- df_ocha %>%
+  left_join(df_pcodes,
+    by = "adm2_en",
+  ) %>%
+  relocate(adm1_pcode:adm2_pcode, .before = adm2_en) %>%
+  mutate(
+    adm0_en = "Nigeria",
+    adm0_pcode = "nga",
+    .before = adm1_pcode,
+  )
+
+# write_csv(
+#   df_nga,
+#   file_paths$save_path
+# )
