e4fe0e4eda754502461b1a9374a2524fe0bd6bc0
Rawa Ganjani <rawaomer@outlook.com>
2022-05-16 19:32:10 +0300

    wrangling yemen data v1.0 (#27)
    * wrangling yemen data v1.0
    
    * yemen data wranling 1.1

---
+++ 01_wrangling/yem.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+# helper functions to get paths
+source(here::here("99_helpers", "helpers.R"))
+
+###################
+#### DATA DIRS ####
+###################
+
+file_paths <- get_paths("Yemen")
+
+############################
+#### OCHA PROVIDED DATA ####
+############################
+
+ocha_fp <- file.path(
+  file_paths$ocha_dir,
+  "JIAF Yemen dataset_consolidated version 6.7_Final.xlsx"
+)
+
+df_ocha_raw <- read_excel(
+  ocha_fp,
+  skip = 3,
+  sheet = "Overall"
+) %>%
+  clean_names()
+
+########################
+#### DATA WRANGLING ####
+########################
+
+#renaming the columns with the same name to their cluster name
+#adm1 pcodes can be extracted from adm2 pcodes
+df_yem <- df_ocha_raw %>%
+  mutate(
+    rmms = sum_row(refugee, migrant, na.rm = T),
+    intersectoral = jiaf_refugee_migrant,
+    protection = total_pi_n_64,
+    wash = total_pi_n_66,
+    shelter = total_pi_n_69,
+    nutrition = total_pi_n_72,
+    education = total_pi_n_75,
+    fsac = total_pi_n_78,
+    cccm = total_pi_n_81,
+    health = total_pi_n_84
+  ) %>%
+  pivot_longer(
+    cols = rmms:health,
+    names_to = "sector",
+    values_to = "pin"
+  ) %>% 
+  group_by(
+    x2,
+    pcode,
+    district,
+    sector
+  ) %>%
+  summarize(
+    pin = sum(pin, na.rm = T)
+  ) %>%
+  transmute(
+    adm0_en = "Yemen",
+    adm0_pcode = "YEM",
+    adm1_name = x2,
+    adm1_pcode = gsub("[0-9]{2}$", "", pcode),
+    amd2_name = district,
+    adm2_pcode = pcode,
+    sector,
+    pin = round(replace_na(pin, 0)),
+    source = "ocha",
+    sector_general = ifelse(sector == "intersectoral", "intersectoral", "sectoral")
+  )
+
+write_csv(
+  df_yem,
+  file_paths$save_path
+)
+
