22371072808aa059b3f2d6de03a766e55b556348
Seth Caldwell <caldwellst@gmail.com>
2022-06-15 21:28:53 +0100

    Add new analyses

--- 04_testing/max_pin_adjustment.R
+++ 04_testing/max_pin_adjustment.R
@@ -99,31 +99,35 @@ df_msna <- read_csv(
   )
 
 ########################
-### Analyse datasets ###
+### ANALYSE DATASETS ###
 ########################
 
 msna_sectors <- unique(df_msna$sector)
 
 df_max_sectors <- df_sectors %>%
   filter(adm0_pcode %in% unique(df_msna$adm0_pcode)) %>%
-  group_by(adm0_pcode,
+  group_by(
+    adm0_pcode,
     adm_name,
     affected_population,
     .drop = TRUE
+  ) %>%
+  mutate(
+    pin_2_std_dev = pin / (mean(pin) + 2 * sd(pin))
   ) %>%
   slice_max(
     order_by = pin,
     n = 2,
     with_ties = FALSE
   ) %>%
-  mutate(
+  summarize(
     max_pin = pin[1],
     max_sector = sector[1],
     max_2_pin = pin[2],
-    max_2_sector = sector[2]
+    max_2_sector = sector[2],
+    max_2_std_dev = pin_2_std_dev[1],
+    .groups = "drop"
   ) %>%
-  select(-c(sector, pin)) %>%
-  unique() %>%
   mutate(
     max_sector = ifelse(grepl("Protection", max_sector),
       "Protection",
@@ -177,6 +181,7 @@ df_msna_anlyse <- df_msna %>%
     max_pin,
     max_2_sector,
     max_2_pin,
+    max_2_std_dev,
     affected_population,
     weight
   ) %>%
@@ -216,6 +221,7 @@ df_msna_anlyse <- df_msna %>%
     max_pin,
     max_2_sector,
     max_2_pin,
+    max_2_std_dev,
     affected_population
   ) %>%
   summarize(
@@ -282,6 +288,17 @@ df_msna_anlyse <- df_msna %>%
     )
   )
 
+temp <- df_msna_anlyse %>%
+  group_by(adm0_pcode) %>%
+  summarise(
+    max_pin = sum(round(max_pin)),
+    pin_adj_max_2_match = sum(round(pin_adj_max_2_match)),
+    pin_adj_all_other_match = sum(round(pin_adj_all_other_match)),
+    affected_population = sum(round(affected_population)),
+    .groups = "drop"
+  )
+
+
 write_csv(
   temp,
   file.path(
@@ -293,15 +310,6 @@ write_csv(
   )
 )
 
-temp <- df_msna_anlyse %>%
-  group_by(adm0_pcode) %>%
-  summarise(
-    max_pin = sum(round(max_pin)),
-    pin_adj_max_2_match = sum(round(pin_adj_max_2_match)),
-    pin_adj_all_other_match = sum(round(pin_adj_all_other_match)),
-    affected_population = sum(round(affected_population)),
-    .groups = "drop"
-  )
 
 temp %>%
   pivot_longer(
@@ -313,15 +321,18 @@ temp %>%
       pin_type == "affected_population" ~ "Targeted population",
       pin_type == "max_pin" ~ paste(
         "Max sectoral PiN",
-        "(no adjustment)"
+        "(no adjustment)",
+        sep = "\n"
       ),
       pin_type == "pin_adj_all_other_match" ~ paste(
         "Max sectoral PiN",
-        "(adjusted by non-overlapping needs with all other sectors)"
+        "(adjusted by all other sectors)",
+        sep = "\n"
       ),
       pin_type == "pin_adj_max_2_match" ~ paste(
         "Max sectoral PiN",
-        "(adjusted by non-overlapping needs with second max sector)"
+        "(adjusted by 2nd max sector)",
+        sep = "\n"
       )
     ),
     value = round(value / 1000000, 2)
@@ -344,7 +355,7 @@ temp %>%
     hjust = -.2,
     size = 4
   ) +
-  coord_flip() +
+  coord_flip(clip = "off") +
   labs(
     x = "",
     y = "PiN",
@@ -352,8 +363,8 @@ temp %>%
       "Impact of accounting for overlap on sectoral PiN"
     ),
     subtitle = paste0(
-      "only adjusting areas where the max sectoral PiN was ",
-      "available in the MSNA, to acccount for the non-overlap"
+      "Only adjusting areas where the max sectoral PiN was ",
+      "available in the MSNA"
     )
   ) +
   theme_minimal() +
@@ -362,11 +373,6 @@ temp %>%
       paste0(x, "M")
     },
     expand = expansion(c(0, .2))
-  ) +
-  scale_x_discrete(
-    labels = function(x) {
-      str_wrap(x, width = 35)
-    }
   ) +
   theme(
     plot.title = element_text(
@@ -394,7 +400,8 @@ temp %>%
     strip.text = element_text(
       size = 16,
       family = "Roboto"
-    )
+    ),
+    panel.spacing = unit(1, "lines")
   )
 
 ggsave(
@@ -440,15 +447,18 @@ temp %>%
       pin_type == "affected_population" ~ "Targeted population",
       pin_type == "max_pin" ~ paste(
         "Max sectoral PiN",
-        "(no adjustment)"
+        "(no adjustment)",
+        sep = "\n"
       ),
       pin_type == "pin_adj_all_other_nomatch" ~ paste(
         "Max sectoral PiN",
-        "(adjusted by non-overlapping needs with all other sectors)"
+        "(adjusted by all other sectors)",
+        sep = "\n"
       ),
       pin_type == "pin_adj_max_2_nomatch" ~ paste(
         "Max sectoral PiN",
-        "(adjusted by non-overlapping needs with second max sector)"
+        "(adjusted by 2nd max sector)",
+        sep = "\n"
       )
     ),
     value = round(value / 1000000, 2)
@@ -471,15 +481,13 @@ temp %>%
     hjust = -.2,
     size = 4
   ) +
-  coord_flip() +
+  coord_flip(clip = "off") +
   labs(
     x = "",
     y = "PiN",
-    title = paste0(
-      "Impact of accounting for overlap on sectoral PiN"
-    ),
+    title = "Impact of accounting for overlap on sectoral PiN",
     subtitle = paste0(
-      "adjusting the non-overlapping needs regardless of the ",
+      "Adjusting the non-overlapping needs regardless of the ",
       "availability of the sector"
     )
   ) +
@@ -489,11 +497,6 @@ temp %>%
       paste0(x, "M")
     },
     expand = expansion(c(0, .2))
-  ) +
-  scale_x_discrete(
-    labels = function(x) {
-      str_wrap(x, width = 35)
-    }
   ) +
   theme(
     plot.title = element_text(
@@ -521,7 +524,8 @@ temp %>%
     strip.text = element_text(
       size = 16,
       family = "Roboto"
-    )
+    ),
+    panel.spacing = unit(1, "lines")
   )
 
 ggsave(
@@ -578,23 +582,27 @@ temp %>%
   mutate(
     name = case_when(
       name == "perc_non_matching" ~ "Sectors not matching with MSNA",
-      name == "perc_max_2_match_exceed" ~ paste0(
-        "PiN adjustment from the second highest sector exceeded the ",
-        "affected population (max sector was available the MSNA)"
+      name == "perc_max_2_match_exceed" ~ paste(
+        "Adjusted PiN exceeded the affected population",
+        "(2nd highest PiN, only if max sector in MSNA)",
+        sep = "\n"
       ),
-      name == "perc_max_2_nomatch_exceed" ~ paste0(
-        "PiN adjustment from the second highest sector exceeded the ",
-        "affected population (max sector was not available the MSNA)"
+      name == "perc_max_2_nomatch_exceed" ~ paste(
+        "Adjusted PiN exceeded the affected population",
+        "(2nd highest PiN)",
+        sep = "\n"
       ),
-      name == "perc_other_match_exceed" ~ paste0(
-        "PiN adjustment from any sector exceeded the affected population ",
-        "(max sector was available the MSNA)"
+      name == "perc_other_match_exceed" ~ paste(
+        "Adjusted PiN exceeded the affected population",
+        "(all other PiNs, only if max sector in MSNA)",
+        sep = "\n"
       ),
-      name == "perc_other_nomatch_exceed" ~ paste0(
-        "PiN adjustment from any sector exceeded the affected population ",
-        "(max sector was not available the MSNA)"
+      name == "perc_other_nomatch_exceed" ~ paste(
+        "Adjusted PiN exceeded the affected population",
+        "(all other PiNs)",
+        sep = "\n"
       ),
-      name == "perc_area_not_covered_msna" ~ "MSNA data is not available"
+      name == "perc_area_not_covered_msna" ~ "MSNA data unavailable"
     ),
     value = round(value * 100)
   ) %>% # nolint
@@ -628,10 +636,7 @@ temp %>%
   labs(
     y = "% of areas affected",
     x = "",
-    title = paste0(
-      "% of areas being affected by types of sectoral ",
-      "PiN misalignment with the MSNA"
-    )
+    title = "Dynamics of sectoral PiN adjustment"
   ) +
   scale_x_discrete(
     labels = function(x) {
@@ -679,5 +684,196 @@ ggsave(
     "2022_msna_misalignments.png"
   ),
   height = 8,
-  width = 22
+  width = 15
+)
+
+##############################
+#### OVERLAP AND OUTLIERS ####
+##############################
+
+temp <- df_msna_anlyse %>%
+  mutate(
+    max_2_pct = max_2_pin / max_pin
+  ) %>%
+  select(
+    adm0_pcode:max_2_pin,
+    max_2_pct,
+    perc_2nd_max_disjoint_nomatch
+  )
+
+temp %>%
+  ggplot(
+    aes(
+      x = max_2_pct,
+      y = perc_2nd_max_disjoint_nomatch
+    )
+  ) +
+  geom_point(
+    alpha = 0.3,
+    fill = "#1EBFB3"
+  ) +
+  theme_minimal() +
+  scale_y_continuous(
+    labels = scales::percent_format(1)
+  ) +
+  scale_x_continuous(
+    labels = scales::percent_format(1)
+  ) +
+  labs(
+    x = "2nd max PiN, % of max PiN",
+    y = "% adjustment (2nd max overlap)",
+    title = "Relationship between 2nd max PiN and overlap adjustment",
+    subtitle = paste0(
+      "Adjusting the non-overlapping needs regardless of the ",
+      "availability of the sector"
+    )
+  ) +
+  theme(
+    plot.title = element_text(
+      face = "bold",
+      size = 22,
+      margin = margin(10, 10, 10, 10, "pt"),
+      hjust = 0.5,
+      family = "Roboto"
+    ),
+    plot.background = element_rect(
+      fill = "white"
+    ),
+    axis.text = element_text(
+      face = "bold",
+      size = 10,
+      family = "Roboto"
+    ),
+    axis.title.x = element_text(
+      face = "bold",
+      size = 12,
+      margin = margin(20, 10, 10, 10, "pt"),
+      family = "Roboto"
+    ),
+    strip.text = element_text(
+      size = 16,
+      family = "Roboto"
+    )
+  )
+
+
+ggsave(
+  file.path(
+    file_paths$output_dir,
+    "graphs",
+    "Option 1",
+    "2022_overlap_and_2nd_max.png"
+  ),
+  height = 6,
+  width = 9
+)
+
+write_csv(
+  temp,
+  file.path(
+    file_paths$output_dir,
+    "graphs",
+    "Option 1",
+    "datasets",
+    "2022_overlap_and_2nd_max.csv"
+  )
+)
+
+#####################################
+#### LOOK AT STD DEV AND OVERLAP ####
+#####################################
+
+df_msna_anlyse %>%
+  ggplot(
+    aes(
+      x = max_2_std_dev,
+      y = perc_2nd_max_disjoint_nomatch
+    )
+  ) +
+  geom_rect(
+    xmin = 0.7,
+    xmax = 1,
+    ymin = 0,
+    ymax = 1,
+    fill = "#CCE5F9"
+  ) +
+  geom_rect(
+    xmin = 1,
+    xmax = 1.4,
+    ymin = 0,
+    ymax = 1,
+    fill = "#FCE0DE"
+  ) +
+  geom_point(
+    alpha = 0.3,
+    fill = "#1EBFB3"
+  ) +
+  theme_minimal() +
+  scale_y_continuous(
+    labels = scales::percent_format(1)
+  ) +
+  scale_x_continuous(
+    labels = scales::percent_format(1)
+  ) +
+  labs(
+    x = "Max PiN, % of mean + 2 std. dev. of PiNs",
+    y = "% adjustment (2nd max overlap)",
+    title = "Relationship between 2nd max PiN and overlap adjustment",
+    subtitle = paste0(
+      "Adjusting the non-overlapping needs regardless of the ",
+      "availability of the sector"
+    )
+  ) +
+  theme(
+    plot.title = element_text(
+      face = "bold",
+      size = 22,
+      margin = margin(10, 10, 10, 10, "pt"),
+      hjust = 0.5,
+      family = "Roboto"
+    ),
+    plot.background = element_rect(
+      fill = "white"
+    ),
+    axis.text = element_text(
+      face = "bold",
+      size = 10,
+      family = "Roboto"
+    ),
+    axis.title.x = element_text(
+      face = "bold",
+      size = 12,
+      margin = margin(20, 10, 10, 10, "pt"),
+      family = "Roboto"
+    ),
+    strip.text = element_text(
+      size = 16,
+      family = "Roboto"
+    )
+  ) +
+  geom_text(
+    data = data.frame(
+      x = c(0.71, 1.02),
+      y = 0.7,
+      label = c("Non-outliers", "Outliers")
+    ),
+    mapping = aes(
+      x = x,
+      y = y,
+      label = label
+    ),
+    hjust = 0,
+    fontface = "bold",
+    family = "Roboto"
+  )
+
+ggsave(
+  file.path(
+    file_paths$output_dir,
+    "graphs",
+    "Option 1",
+    "2022_overlap_and_std_dev.png"
+  ),
+  height = 5,
+  width = 9
 )
