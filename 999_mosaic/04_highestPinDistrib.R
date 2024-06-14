4e353bdd25c457290f9d8c28afa4f154e395a840
lanawwas <lanawwas@jouna.org>
2024-06-14 09:23:58 +0200

    Update method 4 to stop at level3

--- 999_mosaic/04_highestPinDistrib.R
+++ 999_mosaic/04_highestPinDistrib.R
@@ -13,7 +13,7 @@ library(tidyr)
 # Read the CSV file
 data <- read.csv("process/modified_data.csv")
 
-# First Step: Select the sector with the highest PiN for Admin.2(Lowest level of Analysis)
+# Step 1: Select the sector with the highest PiN for Admin.2(Lowest level of Analysis)
 highest_pin_sectors <- data %>%
   group_by(Admin.2.Pcode) %>%
   filter(pin == max(pin)) %>%
@@ -22,5 +22,17 @@ highest_pin_sectors <- data %>%
 # Check the structure of the data
 str(highest_pin_sectors)
 
+# Step 2: Combine '1' and '2', and add them to '3'
+highest_pin_sectors_new <- highest_pin_sectors %>%
+  group_by(Admin.2.Pcode) %>%
+  mutate(
+    across(ends_with("3"), ~ .x + coalesce(rowSums(across(ends_with(c("1", "2")))), 0), .names = "{.col}")
+  ) %>%
+  dplyr::select(-ends_with(c("1", "2"))) %>%
+  ungroup()
+
+# Check the structure of the data
+str(highest_pin_sectors_new)
+
 # Save the updated data with percentages to a new CSV file
-write.csv(highest_pin_sectors, "process/4.modified_data_highest_sector_pin.csv", row.names = FALSE)
+write.csv(highest_pin_sectors_new, "process/4.modified_data_highest_sector_pin.csv", row.names = FALSE)
